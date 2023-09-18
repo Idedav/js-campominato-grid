824e39cbc290aeecfafabdba58dd7ef4aaf69ed1
Idedav <xdavide.correa@gmail.com>
2023-09-18 15:14:09 +0200

    [ADD] bonus

--- js/script.js
+++ js/script.js
@@ -1,18 +1,26 @@
 const container = document.querySelector('.dc-container');
-const btnStart = document.getElementById('start')
+const btnStart = document.getElementById('start');
+const difficulty = document.getElementById('difficulty');
 reset();
 
 btnStart.addEventListener('click', function(){
+    reset();
     start();
 })
 
+
 // --------FUNCTIONS----------
 function start(){
-    for(let i = 0; i < 100; i++){
-        const square = createSquare();
-        console.log(square);
+    for(let i = 1; i <= difficulty.value; i++){
+        const square = createSquare(i);
+        if(difficulty.value == 81){
+            square.classList.add('medium');
+        }else if(difficulty.value == 49){
+            square.classList.add('hard');
+        }
         square.addEventListener('click', function(){
-            square.classList.toggle('clicked')
+            this.classList.toggle('clicked')
+            console.log(this._SquareID);
         })
         container.append(square)
     }
@@ -22,8 +30,9 @@ function reset(){
     return container;
 }
 
-function createSquare(){
+function createSquare(index){
     const newSquare = document.createElement('div');
+    newSquare._SquareID = index;
     newSquare.className = 'square'
     return newSquare;
 }
